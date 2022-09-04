05e48cf0324aff807499cea0151a40d3cefb2bc7
Tanjim-Shadman <97250120+shadman-imaginary@users.noreply.github.com>
2022-09-04 16:04:38 +0600

    minor update

--- PaintBallMain/Assets/Scripts/Tests/MouseLookTest.cs
+++ PaintBallMain/Assets/Scripts/Tests/MouseLookTest.cs
@@ -8,6 +8,15 @@ public class MouseLookTest : MonoBehaviour
     [SerializeField] float sensitivityY = 0.5f;
     float mouseX, mouseY;
 
+    [SerializeField]
+    Transform playerCamera;
+
+    [SerializeField] float xClamp = 85f;
+
+    Vector3 targetRotation;
+
+    float xRotation = 0f;
+
     public void RecieveInput(Vector2 mouseInput)
     {
         mouseX = mouseInput.x * sensitivityX;
@@ -17,5 +26,13 @@ public class MouseLookTest : MonoBehaviour
     private void Update()
     {
         transform.Rotate(Vector3.up,mouseX * Time.deltaTime);
+        //transform.Rotate(Vector3.up, mouseY * Time.deltaTime);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -xClamp, xClamp);
+
+        targetRotation = transform.eulerAngles;
+        targetRotation.x = xRotation;
+        playerCamera.eulerAngles = targetRotation;
     }
 }
